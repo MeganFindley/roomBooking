da2ff416d69cd03b08d2dfb9fd1ac1be5bb2e9f3
Megan <megan.findley@live.co.uk>
2020-11-19 18:09:51 +0000

    Style Update and Responsive Design
    Added/ changed a few features and started the responsive design for home/booking pages

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import './CSS/Home.css'
+import './CSS/Home.css';
+import Checkmark from 'react-checkmark'
 
 function Home() {
     const bookings = [];
@@ -11,12 +12,12 @@ function Home() {
 
     const rooms = [];
     const [roomInfo, setRoomInfo] = useState({});
-    const [overlay, setOverlay] = useState({show: false})
-    const pinkStyle = {left: 'calc(100% / 15)', backgroundColor: 'rgb(255, 214, 238)'};
-    const blueStyle = {left: 'calc(100% / 1.55)', backgroundColor: 'rgb(191, 217, 255)'};
-    const orangeStyle = {left: 'calc(100% / 2.8)', backgroundColor: 'rgb(255, 226, 182)'};
+    const [overlay, setOverlay] = useState({ show: false })
+    const pinkStyle = { left: 'calc(100% / 15)', backgroundColor: 'rgb(255, 214, 238)', closeColor2: 'hotpink', closeColor1: 'rgb(255, 214, 238)', closeBorder: '1px solid hotpink' };
+    const blueStyle = { left: 'calc(100% / 1.55)', backgroundColor: 'rgb(191, 217, 255)', closeColor2: 'rgb(78, 114, 255)', closeColor1: 'rgb(191, 217, 255)', closeBorder: '1px solid rgb(78, 114, 255)' };
+    const orangeStyle = { left: 'calc(100% / 2.8)', backgroundColor: 'rgb(255, 226, 182)', closeColor2: 'orange', closeColor1: 'rgb(255, 226, 182)', closeBorder: '1px solid orange' };
 
-    const [overlayStyle, setOverlayStyle] = useState({}); 
+    const [overlayStyle, setOverlayStyle] = useState({});
 
     useEffect(() => {
         const returnBookings = async () => {
@@ -43,31 +44,48 @@ function Home() {
                     rooms.push(item);
                 }));
             console.log(rooms);
-            setRoomInfo({blue: rooms[0], orange: rooms[1], pink: rooms[2]});
+            setRoomInfo({ blue: rooms[0], orange: rooms[1], pink: rooms[2] });
         };
         returnRooms();
 
     }, []);
 
     const toggleOverlay = (e) => {
-        setRoomInfo({ ...roomInfo, viewroom: roomInfo[ e.target.getAttribute('id')]});
-        // console.log(roomInfo.viewroom)
-        if(e.target.getAttribute('id') === 'pink'){
-            setOverlayStyle(pinkStyle)
-        }else if(e.target.getAttribute('id') === 'orange'){
-            setOverlayStyle(orangeStyle)
-        }else if(e.target.getAttribute('id') === 'blue'){
-            setOverlayStyle(blueStyle)
+        if (window.innerWidth < 1025) {
+            setRoomInfo({ ...roomInfo, viewroom: roomInfo[e.target.getAttribute('id')] });
+            // console.log(roomInfo.viewroom)
+            if (e.target.getAttribute('id') === 'pink') {
+                setOverlayStyle(pinkStyle)
+            } else if (e.target.getAttribute('id') === 'orange') {
+                setOverlayStyle(orangeStyle)
+            } else if (e.target.getAttribute('id') === 'blue') {
+                setOverlayStyle(blueStyle)
+            }
+            setOverlay({ show: !overlay.show });
         }
-        setOverlay({show: !overlay.show});
     }
 
+    const mouseOver = (e) => {
+        if (window.innerWidth > 1025) {
+            setRoomInfo({ ...roomInfo, viewroom: roomInfo[e.target.getAttribute('id')] });
+            // console.log(roomInfo.viewroom)
+            if (e.target.getAttribute('id') === 'pink') {
+                setOverlayStyle(pinkStyle)
+            } else if (e.target.getAttribute('id') === 'orange') {
+                setOverlayStyle(orangeStyle)
+            } else if (e.target.getAttribute('id') === 'blue') {
+                setOverlayStyle(blueStyle)
+            }
+            setOverlay({ show: !overlay.show });
+        }
+    }
     return (
         <React.Fragment>
             <div className='homeComp'>
                 <h1>Meetings Scheduled Today:</h1>
+                <h2>{window.innerWidth > 1025 ? 'Hover over ' : 'Click on '} a room name to see the rooms capabilities</h2>
                 <div className='roomTicket pink'>
-                    <h3 id='pink' className='roomName pinkRoom' onMouseEnter={toggleOverlay} onMouseLeave={toggleOverlay} onClick={toggleOverlay}>PINK</h3>
+                    <h3 id='pink' className='roomName pinkRoom' onMouseEnter={mouseOver} onMouseLeave={mouseOver} onClick={toggleOverlay}>PINK</h3>
                     <div className='roomSchedule pinkSchedule'>
                         {meetings.pink.map(meeting => (
                             <div className='meeting'>
@@ -80,7 +98,7 @@ function Home() {
                     </div>
                 </div>
                 <div className='roomTicket orange'>
-                    <h3 id='orange' className='roomName orangeRoom' name='orange' onMouseEnter={toggleOverlay} onMouseLeave={toggleOverlay} onClick={toggleOverlay}>ORANGE</h3>
+                    <h3 id='orange' className='roomName orangeRoom' name='orange' onMouseEnter={mouseOver} onMouseLeave={mouseOver} onClick={toggleOverlay}>ORANGE</h3>
                     <div className='roomSchedule orangeSchedule'>
                         {meetings.orange.map(meeting => (
                             <div className='meeting'>
@@ -93,7 +111,7 @@ function Home() {
                     </div>
                 </div>
                 <div className='roomTicket blue'>
-                    <h3 id='blue' className='roomName blueRoom' onMouseEnter={toggleOverlay} onMouseLeave={toggleOverlay} onClick={toggleOverlay}>BLUE</h3>
+                    <h3 id='blue' className='roomName blueRoom' onMouseEnter={mouseOver} onMouseLeave={mouseOver} onClick={toggleOverlay}>BLUE</h3>
                     <div className='roomSchedule blueSchedule'>
                         {meetings.blue.map(meeting => (
                             <div className='meeting'>
@@ -107,13 +125,14 @@ function Home() {
                 </div>
 
                 {/* room overlay */}
-                <div className='roomOverlay' style={{display: overlay.show ? 'block' : 'none' , left: overlayStyle.left, backgroundColor: overlayStyle.backgroundColor}}>
-                    <h3>{roomInfo.viewroom ? roomInfo.viewroom.roomName : ' '} rooms capabilities</h3>
-                    <p>Capacity is {roomInfo.viewroom ? roomInfo.viewroom.capacity : ' '} people</p>
-                    <p>{roomInfo.viewroom ? (roomInfo.viewroom.phone ? 'Has ' : 'Does not have ') : ' '} a phone</p>
-                    <p>{roomInfo.viewroom ? (roomInfo.viewroom.projector ? 'Has ' : 'Does not have ') : ' '} a projector</p>
-                    <p>{roomInfo.viewroom ? (roomInfo.viewroom.tvScreen ? 'Has ' : 'Does not have ') : ' '} a TV</p>
-                    <p>{roomInfo.viewroom ? (roomInfo.viewroom.wheelchairAccess ? 'Is ' : 'Is not ') : ' '} wheelchair accessible</p>
+                <div className='roomOverlay' style={{ display: overlay.show ? 'block' : 'none', left: overlayStyle.left, backgroundColor: overlayStyle.backgroundColor }}>
+                    <button className='close' style={{ backgroundColor: overlayStyle.closeColor2, color: overlayStyle.closeColor1, border: 'none'}} onClick={toggleOverlay}><span>x</span></button>
+                    <h3 style={{color: overlayStyle.closeColor2}}>{roomInfo.viewroom ? roomInfo.viewroom.roomName.toUpperCase() : ' '} ROOMS CAPABILITIES</h3>
+                    <p>Capacity: <span>{roomInfo.viewroom ? roomInfo.viewroom.capacity :  ' '}</span></p>
+                    <p>Phone: <span>{roomInfo.viewroom ? (roomInfo.viewroom.phone ? '✓' : '✗') : ' '}</span></p>
+                    <p>Projector: <span>{roomInfo.viewroom ? (roomInfo.viewroom.projector ? '✓' : '✗') : ' '}</span></p>
+                    <p>Tv: <span>{roomInfo.viewroom ? (roomInfo.viewroom.tvScreen ? '✓' : '✗') : ' '}</span></p>
+                    <p>Wheelchair Access: <span>{roomInfo.viewroom ? (roomInfo.viewroom.wheelchairAccess ? '✓' : '✗') : ' '}</span></p>
                 </div>
             </div>
         </React.Fragment>
