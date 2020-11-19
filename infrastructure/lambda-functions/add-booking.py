9ba5061c60b6f5784c18e99708f094ca7182a55d
Luke <lstobbart35@gmail.com>
2020-11-19 10:02:15 +0000

    fixed error handling for double booking meetings

--- infrastructure/lambda-functions/add-booking.py
+++ infrastructure/lambda-functions/add-booking.py
@@ -1,7 +1,26 @@
 import json
 import boto3
 from boto3.dynamodb.conditions import Key
+import datetime
+from datetime import datetime, timedelta, date
 dynamodb = boto3.resource('dynamodb', region_name='eu-west-2')
+
+def calculate_end_time(initialTime, mins):
+	end_time = datetime(100, 1, 1, initialTime.hour, initialTime.minute, initialTime.second)
+	end_time = end_time + timedelta(minutes=mins)
+	return end_time.time()
+	
+def is_overlap(requested_end_time, booked_end_time, requested_start_time, booked_start_time):
+	requested_end_time = datetime.combine(date.min, requested_end_time)
+	booked_end_time = datetime.combine(date.min, booked_end_time)
+	requested_start_time = datetime.combine(date.min, requested_start_time)
+	booked_start_time = datetime.combine(date.min, booked_start_time)
+	delta = min(booked_end_time, requested_end_time)-max(booked_start_time, requested_start_time)
+	print(delta.total_seconds())
+	if delta.total_seconds() < 0:
+		return False
+	else:
+		return True
 
 def query_booking(event):
 	table = dynamodb.Table('Room-Bookings')
@@ -9,11 +28,18 @@ def query_booking(event):
 		KeyConditionExpression=Key('roomName').eq(event['queryStringParameters']['roomName'])
 	)
 	bookings = response['Items']
+	requested_start_time = datetime.strptime(event['queryStringParameters']['time'], '%H:%M').time()
+	requested_end_time = calculate_end_time(requested_start_time, int(event['queryStringParameters']['duration']))
+	list1 = []
 	for booking in bookings:
-		if booking['date'] == event['queryStringParameters']['date'] and booking['time'] == event['queryStringParameters']['time']:
-			return True
-		else:
-			return False
+		booked_start_time = datetime.strptime(booking['time'], '%H:%M').time()
+		booked_end_time = calculate_end_time(booked_start_time, int(booking['duration']))
+		list1.append(is_overlap(requested_end_time, booked_end_time, requested_start_time, booked_start_time))
+	print(list1)
+	if list1.count(True) > 0:
+		return True
+	else:
+		return False
 
 def query_room(event):
     table = dynamodb.Table('Meeting-Rooms')
@@ -21,14 +47,10 @@ def query_room(event):
         KeyConditionExpression=Key('roomName').eq(event['queryStringParameters']['roomName'])
     )
     return response['Items']
-    
 
 def lambda_handler(event, context):
 	room = query_room(event)
-	print(query_booking(event))
 	isBooking = query_booking(event)
-	#print(room[0]['capacity'])
-	#print(event['queryStringParameters']['capacity'])
 	if isBooking:
 		return {
 			'body': json.dumps({'Message': "Sorry! This room already has a meeting booked at this time."})
